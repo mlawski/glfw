f38d568f82d6ae7c405bf93e9f46d13cc171244a
Marcin Lawski <lawski.marcin@gmail.com>
2026-02-15 12:35:36 +0100

    Make bash script executable

--- createLinuxFastbuild.sh
+++ createLinuxFastbuild.sh
@@ -2,11 +2,11 @@
 rm -rf build
 
 # --- DEBUG: static CRT ---
-cmake . -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreadedDebug" -B "build/debug"
+cmake . -B "build/debug"
 cmake --build "build/debug" --config Debug
 
 # --- RELEASE: static CRT ---
-cmake . -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" -B "build/release"
+cmake . -B "build/release"
 cmake --build "build/release" --config Release
 
 echo "Build complete!"
